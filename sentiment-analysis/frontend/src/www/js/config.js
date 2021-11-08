7c1f8d31eea1c02641c2b8c1a71c4c40aa9d4ea8
kyleblocksom <73256380+kyleblocksom@users.noreply.github.com>
2021-11-08 11:29:52 -0800

    Add files via upload

---
+++ sentiment-analysis/frontend/src/www/js/config.js
@@ -0,0 +1,14 @@
+
+const config = {
+    auth: {
+        uri: "https://frontend-2.auth.us-east-1.amazoncognito.com",
+        clientId: "7lp1pp8ghkr7i4ata98dj7f0uc",
+    },
+    api: {
+        pageSize: 25,
+        uri: "https://c9zf53lujh.execute-api.us-east-1.amazonaws.com/Prod",
+    },
+    dashboard: {
+        uri: "https://us-east-1.quicksight.aws.amazon.com/sn/start",
+    },
+};
